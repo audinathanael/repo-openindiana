54746fcb74754550772abe7980877d582e8afdf2
audi ubuntu <audinathanael@gmail.com>
2023-01-17 13:56:58 +0700

    fix minor in publisher

--- convert.py
+++ convert.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Read the Excel file into a dataframe
-df = pd.read_excel("package_data5.xlsx")
+df = pd.read_excel("package_data.xlsx")
 
-df= df["name","version"]
 
-df['Name'] = 'pkgrecv -s http://localhost:10000 -d /repo3 pkg://openindiana.org/' + df['Name']+"@"+df["Version"]
+
+df['Name'] = 'pkgrecv -s http://pkg.openindiana.org/dev -d /repo3 pkg://openindiana.org/' + df['Name']+"@"+df["Version"]
 
 df.drop("Version", axis=1, inplace=True)
 data= df.to_string(index=False,header=False)
@@ -13,6 +13,6 @@ data= df.to_string(index=False,header=False)
 # data_with_text = data.replace("\n","df['Name'] = 'pkg://openindiana.org/' + df['Name']+'/'+df['Version']")
 
 # # Write the string to a text file
-with open("0.5.11,5.11-0.151.1.4.txt", "w") as f:
+with open("0.5.11,5.11-0.151.1.9.txt", "w") as f:
         f.write(data)
     
