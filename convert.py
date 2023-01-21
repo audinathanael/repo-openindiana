dae1fd2dd7e061bacc17a2227e52069277414e4b
audinathanael <audinathanael@gmail.com>
2023-01-21 14:41:55 +0700

    fix total package solaris and script testing solaris

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ df = pd.read_excel("package solaris/86.xlsx")
 
 
 
-df['Name'] = 'pkgrecv -s http://pkg.openindiana.org/legacy -d /repo3 pkg://openindiana.org/' + df['Name']+"@"+df["Version"]
+df['Name'] = 'pkgrecv -s http://pkg.openindiana.org/legacy -d /repo3 pkg://opensolaris.org/' + df['Name']+"@"+df["Version"]
 
 df.drop("Version", axis=1, inplace=True)
 data= df.to_string(index=False,header=False)
