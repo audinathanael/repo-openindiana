5956c8a45efee50f3ea80641654300e4bf431ef6
audinathanael <audinathanael@gmail.com>
2023-01-21 11:42:18 +0700

    testing script solaris

--- convert.py
+++ convert.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Read the Excel file into a dataframe
-df = pd.read_excel("package openindiana/148.xlsx")
+df = pd.read_excel("package solaris/86.xlsx")
 
 
 
-df['Name'] = 'pkgrecv -s http://pkg.openindiana.org/dev -d /repo3 pkg://openindiana.org/' + df['Name']+"@"+df["Version"]
+df['Name'] = 'pkgrecv -s http://pkg.openindiana.org/legacy -d /repo3 pkg://openindiana.org/' + df['Name']+"@"+df["Version"]
 
 df.drop("Version", axis=1, inplace=True)
 data= df.to_string(index=False,header=False)
@@ -13,6 +13,6 @@ data= df.to_string(index=False,header=False)
 # data_with_text = data.replace("\n","df['Name'] = 'pkg://openindiana.org/' + df['Name']+'/'+df['Version']")
 
 # # Write the string to a text file
-with open("script148.txt", "a") as f:
+with open("scriptSolaris.txt", "a") as f:
         f.writelines([data])
     
