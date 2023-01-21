cfa6f966b642555ee779e8b4679e2ceffadee34a
audinathanael <audinathanael@gmail.com>
2023-01-21 10:57:13 +0700

    fix rename package openindiana

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Read the Excel file into a dataframe
-df = pd.read_excel("package openindiana/package_data10.xlsx")
+df = pd.read_excel("package openindiana/148.xlsx")
 
 
 
@@ -13,6 +13,6 @@ data= df.to_string(index=False,header=False)
 # data_with_text = data.replace("\n","df['Name'] = 'pkg://openindiana.org/' + df['Name']+'/'+df['Version']")
 
 # # Write the string to a text file
-with open("script.txt", "a") as f:
+with open("script148.txt", "a") as f:
         f.writelines([data])
     
